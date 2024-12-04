eb41abae812d1e7c30962281cf48005f2c2cb87a
Riyan Amanda <ryant.n92@gmail.com>
2024-12-04 22:45:42 +0700

    feat: initial backend

---
+++ server/src/main/java/com/ran/server/SistemManagemenAsetPerusahaanApplication.java
@@ -0,0 +1,13 @@
+package com.ran.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemManagemenAsetPerusahaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemManagemenAsetPerusahaanApplication.class, args);
+	}
+
+}
