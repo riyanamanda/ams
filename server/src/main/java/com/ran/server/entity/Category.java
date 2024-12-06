2282fe38812e7d470f3fb7e022d1f3e4d4a38fb8
Riyan Amanda <ryant.n92@gmail.com>
2024-12-06 21:53:51 +0700

    feat: add env setting and category entity

---
+++ server/src/main/java/com/ran/server/entity/Category.java
@@ -0,0 +1,38 @@
+package com.ran.server.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Riyan Amanda
+ * @linkedin <a href="https://linkedin.com/in/riyan-amanda">...</a>
+ * @since 06/12/2024, Friday
+ **/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", insertable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
